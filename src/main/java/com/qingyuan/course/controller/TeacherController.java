79dc8984a76b9127ee5c7b9a85f2157986947a05
guoshusong <2460526449@qq.com>
2020-10-22 21:52:06 +0800

    done:数据库设计

---
+++ src/main/java/com/qingyuan/course/controller/TeacherController.java
@@ -0,0 +1,13 @@
+package com.qingyuan.course.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 教师相关接口
+ * @program: course
+ * @author: GuoShuSong
+ * @create: 2020-10-22 21:38
+ **/
+@RestController
+public class TeacherController {
+}
---
+++ src/main/java/com/qingyuan/course/service/CourseService.java
@@ -0,0 +1,8 @@
+package com.qingyuan.course.service;
+
+/**
+ *课程相关接口
+ * @author 24605
+ */
+public interface CourseService {
+}
